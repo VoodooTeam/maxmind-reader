3663996213b5e82c73716f0049daa6f612a30afe
Vincent Vallet <vincentvallet@voodoo.io>
2019-08-13 15:29:52 +0200

    Add tests, linter and complete readme.

--- README.md
+++ README.md
@@ -21,6 +21,7 @@ Supported and tested : >= 7.6
 
 | Version       | Supported     | Tested         |
 | ------------- |:-------------:|:--------------:|
+| 12.x          | yes           | yes            |
 | 10.x          | yes           | yes            |
 | 9.x           | yes           | yes            |
 | 8.x           | yes           | yes            |
@@ -34,8 +35,37 @@ $ npm install @voodoo.io/maxmind-reader --save
 
 # Usage
 
-## Basic usage
+## Instantiation & init
 
+```javascript
+const S3Tools = require('@voodoo.io/aws-utils').s3
+const s3 = new aws.S3()
+const s3Tools = new S3Tools(s3)
+
+const Geoloc = require('@voodoo.io/maxmind-reader')
+const geoloc = new Geoloc({
+  // here is your config
+}, s3Tools)
+
+// launch the interval to reload the file
+// the delay can be customized as parameter
+await geoloc.init()
+```
+
+## Basic usage
+```javascript
+const isoCode = geoloc.getCountry('149.62.156.82')
+// isoCode equals 'FR'
+```
+
+## Config
+
+| Property           | description                              | Default value  |
+| -------------------|:----------------------------------------:|:--------------:|
+| `S3_GEOLOC_KEY`    | '/path/to/GeoLite2-Country.mmdb'         | empty          |
+| `S3_GEOLOC_BUCKET` | Bucket's name                            | empty          |
+| `delay`            | Interval in ms between two reloading     | 3600000 (1hour)|
+| `enableScheduler`  | Enable the scheduler?                    | true           |
 
 # Test
 
--- src/maxmind_reader.js
+++ src/maxmind_reader.js
@@ -1,26 +1,21 @@
 const { Reader } = require('maxmind')
 const retry = require('async-await-retry')
-const path = require('path')
 
 class MaxmindReader {
-
     /**
      * Retrieve maxmind db
-     * 
+     *
      * @param {Object} config : global module oconfiguration
      * @property {Boolean} config.enableScheduler : do we need to retrieve db file periodically?
      * @property {Number} config.delay : interval (in ms) for scheduler
      * @param {class} s3Tools - S3 helper
      */
-    constructor(config, s3Tools) {
+    constructor (config, s3Tools) {
         this.s3Tools = s3Tools
         this.delay = config.delay || 1000
         this.enableScheduler = config.enableScheduler || true
         this.S3_GEOLOC_KEY = config.S3_GEOLOC_KEY
         this.S3_GEOLOC_BUCKET = config.S3_GEOLOC_BUCKET
-
-        this.resPath = config.path || path.join(__dirname, '../resources');
-        this.filePath = path.join(this.resPath, this.S3_GEOLOC_KEY);
     }
 
     /**
@@ -28,19 +23,19 @@ class MaxmindReader {
      *
      */
     async init () {
-        await this.safeUpdateGeolocDb();
-        if(this.enableScheduler) {
+        await this.safeUpdateGeolocDb()
+        if (this.enableScheduler) {
             this.timer = setInterval(async () => {
-                await this.safeUpdateGeolocDb(); 
-            }, this.delay);
+                await this.safeUpdateGeolocDb()
+            }, this.delay)
         }
     }
 
-     /**
+    /**
      * Clean timer
      *
      */
-    destroy() {
+    destroy () {
         clearInterval(this.timer)
     }
 
@@ -48,24 +43,24 @@ class MaxmindReader {
      * Get country from IP from geoloc database.
      *
      */
-    getCountry(ip) {
+    getCountry (ip) {
         if (!ip) {
-            return null;
+            return null
         }
-        const info = this.countryLookup.get(ip);
-        return info && info.country ? info.country.iso_code : null;
+        const info = this.countryLookup.get(ip)
+        return info && info.country ? info.country.iso_code : null
     }
 
     /**
      * Update geoloc db.
      *
      */
-    async updateGeolocDb() {
+    async updateGeolocDb () {
         const buffer = await this.s3Tools.getObject(
             this.S3_GEOLOC_BUCKET,
             this.S3_GEOLOC_KEY,
             'buffer'
-        );
+        )
         this.countryLookup = new Reader(buffer)
     }
 
@@ -75,9 +70,9 @@ class MaxmindReader {
      */
     async safeUpdateGeolocDb () {
         try {
-            await this.updateGeolocDb();
+            await this.updateGeolocDb()
         } catch (err) {
-            await retry(this.updateGeolocDb.bind(this), [], 10);
+            await retry(this.updateGeolocDb.bind(this), [], 10)
         }
     }
 }
